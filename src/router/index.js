ce5d435283a6d26892681c1c87ed8371e7412ce8
DuyenTruong <truongthimyduyen27042001@gmail.com>
2022-10-19 09:52:39 +0700

    vue create project

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/admin/Category/add',
+    name: 'AddCategory',
+    component: () => import('../views/Category/AddCategory.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+})
+
+export default router
